ec445fc44ec1e903168320d9d93b7ca3454a6c50
Benjamin Goldstein <benjaming@d-fendsolutions.com>
2023-01-28 22:23:09 +0200

    feat(server): Add body parser to server app

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const PORT = 3000;
+
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
     console.log(`Listening at port ${PORT}`);
